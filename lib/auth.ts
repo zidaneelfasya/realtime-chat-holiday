7416b44ba19b62fc5d0e0ed9ff18a6c757a6b97e
rawhx_x <achmadhasbil24@gmail.com>
2025-01-29 08:22:56 +0700

    verify Api

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,5 @@
 import { jwtVerify } from "jose";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const secretKey = new TextEncoder().encode(process.env.JWT_SECRET || "!@#$%^&*()");
 
@@ -9,5 +10,19 @@ export async function verifyToken(token: string) {
   } catch (error) {
     console.error("Invalid token:", error);
     throw new Error("Invalid or expired token")
+  }
+}
+export async function verifyAPI(req: any, res: NextApiResponse) {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ message: "Authorization header is missing" });
+  }
+
+  try {
+    const token = authorization.split(" ")[1];
+    const decode = await jwtVerify(token, secretKey); 
+    req.user = decode 
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token", error });
   }
 }
