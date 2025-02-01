27b5da0bfccb725e39909b6530dcc39a499c3ad4
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-02-01 21:30:42 +0700

    Create HeaderChat.tsx

---
+++ components/HeaderChat.tsx
@@ -0,0 +1,42 @@
+import { FC } from "react";
+
+interface HeaderChatProps {
+  name: string;
+}
+
+const HeaderChat: FC<HeaderChatProps> = ({ name }) => {
+  return (
+    <nav className=" dark:bg-gray-800 rounded-xl">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            {name}
+          </span>
+        </a>
+        <button
+          type="button"
+          className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default HeaderChat;
