b1a9fe14fa2dd7d72921c8f610db288478fc8287
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-02-01 21:30:44 +0700

    Create ListFriend.tsx

---
+++ components/ListFriend.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+
+interface ListFriendProps {
+  name: string;
+  text: string;
+}
+
+const ListFriend: FC<ListFriendProps> = ({ name, text }) => {
+  return (
+    <div className="p-2 bg-gray-700 rounded-lg my-2 ">
+      <div>
+        <p className="text-lg text-gray-300">{name}</p>
+        <p className="text-lg text-gray-300">yesterday</p>
+      </div>
+      <div>
+        <p className="text-sm text-white">{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ListFriend;
