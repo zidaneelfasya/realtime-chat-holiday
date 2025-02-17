65bb5458668aa17ece96e272fb388f18f0244510
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-02-17 20:28:22 +0700

    Update ChatInput.tsx

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -2,21 +2,24 @@ import { useState } from "react";
 import HELPER from "@/helpers/helper";
 
 interface ChatInputProps {
-  setMessages: React.Dispatch<React.SetStateAction<{ _id: string; text: string; sender: string }[]>>;
+  setMessages: React.Dispatch<
+    React.SetStateAction<{ _id: string; text: string; sender: string }[]>
+  >;
+  selectedFriend: { _id: string } | null;
 }
 
-export default function ChatInput({ setMessages }: ChatInputProps) {
+export default function ChatInput({ setMessages , selectedFriend}: ChatInputProps) {
   const [message, setMessage] = useState("");
 
   const sendMessage = async () => {
-    if (!message.trim()) return;
+    if (!message.trim() || !selectedFriend) return;
 
     try {
       const res = await HELPER.Axios("POST", "/api/message", {
-        text: message,
-        sender: "You", // Sesuaikan ini dengan sistem autentikasi user-mu
+        content: message,
+        receiver: selectedFriend._id, // isi bagian ini dengan dengan id sender
       });
-
+      console.log(selectedFriend._id)
       setMessages((prevMessages) => [...prevMessages, res.data.data]);
       setMessage("");
     } catch (error) {
