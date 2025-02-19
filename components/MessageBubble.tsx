2d365996e2a866c73a3e840db4c4ea61d4c9c069
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-02-19 19:28:03 +0700

    Update MessageBubble.tsx

--- components/MessageBubble.tsx
+++ components/MessageBubble.tsx
@@ -1,13 +1,31 @@
+
 interface MessageProps {
   text: string;
-  sender: string;
+  id_sender: string;
+  sender: {
+    _id: string;
+    username: string;
+  };
 }
-
-export default function MessageBubble({ text, sender }: MessageProps) {
+export default function MessageBubble({ text, sender, id_sender }: MessageProps) {
+  const isSender = id_sender === sender._id;
+  console.log(id_sender)
   return (
-    <div className="p-2 bg-gray-700 rounded-lg my-2">
-      <p className="text-sm text-gray-300">{sender}</p>
-      <p className="text-white">{text}</p>
+    <div
+      className={`flex w-full ${
+        isSender ? "justify-end" : "justify-start"
+      } my-1`}
+    >
+      <div
+        className={`p-3 rounded-lg max-w-[75%] break-words ${
+          isSender
+            ? "bg-blue-500 text-white self-end"
+            : "bg-gray-700 text-white self-start"
+        }`}
+      >
+        
+        <p>{text}</p>
+      </div>
     </div>
   );
 }
