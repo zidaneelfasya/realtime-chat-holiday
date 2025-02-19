1dd346744b54781a550ed17c1ea177542aadc247
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-02-19 19:28:05 +0700

    Update login.ts

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -5,9 +5,14 @@ import User from "../../../models/User";
 import { SignJWT } from "jose";
 import { serialize } from "cookie";
 
-const secretKey = new TextEncoder().encode(process.env.JWT_SECRET || "!@#$%^&*()");
+const secretKey = new TextEncoder().encode(
+  process.env.JWT_SECRET || "!@#$%^&*()"
+);
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
@@ -15,21 +20,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { username, password } = req.body;
 
   if (!username || !password) {
-    return res.status(400).json({ message: "Username and password are required" });
+    return res
+      .status(400)
+      .json({ message: "Username and password are required" });
   }
 
   await connectToDatabase();
 
   try {
-    const existingUser = await User.findOne({ username })
-    const isPasswordValid = await compare(password, existingUser.password)
+    const existingUser = await User.findOne({ username });
+    const isPasswordValid = await compare(password, existingUser.password);
     if (!existingUser || !isPasswordValid) {
       return res.status(401).json({ message: "Invalid username or password" });
     }
 
-    const token = await new SignJWT({ 
-      id: existingUser._id, 
-      username: existingUser.username 
+    const token = await new SignJWT({
+      id: existingUser._id,
+      username: existingUser.username,
     })
       .setProtectedHeader({ alg: "HS256" })
       .setIssuedAt()
@@ -43,17 +50,26 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       path: "/",
       maxAge: 24 * 60 * 60,
     });
-
-    res.setHeader("Set-Cookie", cookie);
-
+    
+    const id = serialize("idUser", existingUser._id, {
+      httpOnly: false,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+      maxAge: 24 * 60 * 60,
+    });
+    res.setHeader("Set-Cookie", [cookie, id]);
     res.status(200).json({
       message: "Login successful",
+
       data: {
-        id: existingUser._id, 
-        username, 
+        id: existingUser._id,
+        username,
       },
-    })
+    });
   } catch (error) {
-    return res.status(500).json({ message: "Something went wrong", error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Something went wrong", error: error.message });
   }
 }
